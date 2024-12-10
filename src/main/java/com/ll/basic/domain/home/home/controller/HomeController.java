10a71ed15201871cf258fbc467c67d8474117461
baekminjin <bhs3392@naver.com>
2024-12-10 13:56:10 +0900

    data type

--- src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
@@ -4,11 +4,90 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class HomeController {
+	private int age = 22;
+
 	@GetMapping("/")
-	@ResponseBody //응답값이 고객에게 보여진다.
-	public String showmain(){
-		return "안녕하세요";
+	@ResponseBody
+	public String showMain() {
+		return "안녕하세요.";
+	}
+
+	@GetMapping("/about")
+	@ResponseBody
+	public String showAbout() {
+		return "저는 홍길동입니다.";
+	}
+
+	@GetMapping("/ageUp")
+	@ResponseBody
+	public int getAgeAndUp() {
+		return age++;
+	}
+
+	@GetMapping("/boolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return true;
+	}
+
+	@GetMapping("/byte")
+	@ResponseBody
+	public byte getByte() {
+		return 127;
+	}
+
+	@GetMapping("/short")
+	@ResponseBody
+	public short getShort() {
+		return 32000;
+	}
+
+	@GetMapping("/long")
+	@ResponseBody
+	public long getLong() {
+		return 100_000_000_000_000L;
+	}
+
+	@GetMapping("/char")
+	@ResponseBody
+	public char getChar() {
+		return '꽑';
+	}
+
+	@GetMapping("/float")
+	@ResponseBody
+	public float getFloat() {
+		return 3.14f;
+	}
+
+	@GetMapping("/double")
+	@ResponseBody
+	public double getDouble() {
+		return 3.141592;
+	}
+
+	@GetMapping("/array")
+	@ResponseBody
+	public String[] getArray() {
+		String[] arr = {"a", "b", "c"};
+
+		return arr;
+	}
+
+	@GetMapping("/list")
+	@ResponseBody
+	public List<String> getList() {
+		return List.of("a", "b", "c");
+	}
+
+	@GetMapping("/map")
+	@ResponseBody
+	public Map<String, String> getMap() {
+		return Map.of("name", "Paul", "hobby", "reading");
 	}
 }
