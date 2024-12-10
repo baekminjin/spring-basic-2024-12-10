99e2abf8f79f1435599768af465b4b9ed12db5c6
baekminjin <bhs3392@naver.com>
2024-12-10 16:29:32 +0900

    RequiredArgsConstructor, @RestController

--- src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -2,6 +2,7 @@ package com.ll.basic.domain.wiseSaying.wiseSaying.controller;
 
 import com.ll.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
 import com.ll.basic.domain.wiseSaying.wiseSaying.service.WiseSayingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,13 +11,17 @@ import java.util.List;
 
 @Controller
 @ResponseBody //여기에 붙이면 모든 메소드에 적용된다.
+@RequiredArgsConstructor //final 붙은 것을 자동으로 생성자 생서해주는 것
  public class WiseSayingController {
 	private final WiseSayingService wiseSayingService;
 
+	/*
 	//@Autowired // Component new WiseSayingService <- 스프링 부트가 해준다. // 생략이 가능하다.
 	public WiseSayingController(WiseSayingService wiseSayingService) {
 		this.wiseSayingService = wiseSayingService;
 	}
+	*/
+
 
 	@GetMapping("/wiseSayings")
 
