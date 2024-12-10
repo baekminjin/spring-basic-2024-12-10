3eb4d613179c556325c237accada505325f19624
baekminjin <bhs3392@naver.com>
2024-12-10 14:58:17 +0900

    articleList.html

--- src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
@@ -121,10 +122,27 @@ public class HomeController {
 				"article2", Article.builder().title("제목2").body("내용2").build()
 		);
 	}
+
+	@GetMapping("/articleList.html")
+	@ResponseBody
+	public String getArticlesDotHtml() {
+		Article article1 = Article.builder().id(1).title("제목1").body("내용1").build();
+		Article article2 = Article.builder().id(2).title("제목2").body("내용2").build();
+		Article article3 = Article.builder().id(3).title("제목3").body("내용3").build();
+		Article article4 = Article.builder().id(4).title("제목4").body("내용4").build();
+
+		List<Article> articles = List.of(article1, article2, article3, article4);
+
+		String lis = articles
+				.stream()
+				.map(article -> "<li>%d번 / %s</li>".formatted(article.getId(), article.getTitle()))
+				.collect(Collectors.joining("\n"));
+
+		return "<ul style=\"font-weight: bold; color: red;\">\n" + lis + "\n</ul>";
+	}
+
 }
 
-//@AllArgsConstructor//전부
-//@RequiredArgsConstructor //필수인 것만 받고 싶다 (final)
 @Builder
 @Getter //public으로 안해도 된다.
 class Article{
@@ -138,3 +156,6 @@ class Article{
 	private boolean published = true;
 
 }
+
+//@AllArgsConstructor//전부
+//@RequiredArgsConstructor //필수인 것만 받고 싶다 (final)
