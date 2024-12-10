43e035c029352b9486a9728608c82ea159787f77
baekminjin <bhs3392@naver.com>
2024-12-10 16:22:35 +0900

    autowired, component

--- src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -2,6 +2,7 @@ package com.ll.basic.domain.wiseSaying.wiseSaying.controller;
 
 import com.ll.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
 import com.ll.basic.domain.wiseSaying.wiseSaying.service.WiseSayingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,15 +10,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
-public class WiseSayingController {
+@ResponseBody //여기에 붙이면 모든 메소드에 적용된다.
+ public class WiseSayingController {
 	private final WiseSayingService wiseSayingService;
 
-	public WiseSayingController() {
-		this.wiseSayingService = new WiseSayingService();
+	@Autowired //new WiseSayingService <- 스프링 부트가 해준다. // 받으면 된다.
+	public WiseSayingController(WiseSayingService wiseSayingService) {
+		this.wiseSayingService = wiseSayingService;
 	}
 
 	@GetMapping("/wiseSayings")
-	@ResponseBody
+
 	public List<WiseSaying> getItems() {
 		return wiseSayingService.findAll();
 	}
