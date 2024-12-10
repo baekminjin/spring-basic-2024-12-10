b3fe5ad038b9a39c9aee9aa2d903cc4519eadd02
baekminjin <bhs3392@naver.com>
2024-12-10 16:35:03 +0900

    write -1

--- src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -3,14 +3,14 @@ package com.ll.basic.domain.wiseSaying.wiseSaying.controller;
 import com.ll.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
 import com.ll.basic.domain.wiseSaying.wiseSaying.service.WiseSayingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@ResponseBody //여기에 붙이면 모든 메소드에 적용된다.
+@RestController //@ResponseBody와 @Controller 합친 것
+//@ResponseBody //여기에 붙이면 모든 메소드에 적용된다.
 @RequiredArgsConstructor //final 붙은 것을 자동으로 생성자 생서해주는 것
  public class WiseSayingController {
 	private final WiseSayingService wiseSayingService;
@@ -23,9 +23,22 @@ import java.util.List;
 	*/
 
 
-	@GetMapping("/wiseSayings")
+	@GetMapping("/wiseSayings") //목록
 
 	public List<WiseSaying> getItems() {
 		return wiseSayingService.findAll();
 	}
+
+
+	@GetMapping("/wiseSayings/write") //등록
+	public WiseSaying write(
+			// ? 뒤의 content 파라미터 값이 들어온다.
+			@RequestParam(name = "content") String content,
+			// ? 뒤의 author 파라미터 값이 들어온다.
+			@RequestParam(name = "author") String author
+	)
+	{
+		return wiseSayingService.write(content, author);
+	}
+
 }
